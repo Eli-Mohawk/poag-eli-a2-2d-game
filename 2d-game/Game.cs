e859249630be85fa11a97e7e22f5e322c5a14d40
Eli-Mohawk <eli.poag@mohawkcollege.ca>
2025-09-30 18:55:37 -0400

    stuff

--- 2d-game/Game.cs
+++ 2d-game/Game.cs
@@ -6,6 +6,8 @@ namespace MohawkGame2D
 {
     public class Game
     {
+        Color backgroundMain = new Color(130, 140, 205);
+        Color backgroundShine = new Color(150, 160, 205);
         public void Setup()
         {
             Window.SetTitle("INSERT NAME HERE");
@@ -14,9 +16,27 @@ namespace MohawkGame2D
         }
         public void Update()
         {
+            Window.ClearBackground(Color.White);
 
+            Background();
         }
-      
+        void Background()
+        {
+            // draws the background and the purple outline
+            Draw.LineSize = 50;
+            Draw.LineColor = backgroundMain;
+            Draw.FillColor = Color.Black;
+            Draw.Rectangle(new Vector2(0, 0), new Vector2(600, 800));
+
+            for (int brick = 0; brick < 35; brick++)
+            {
+                int brickGapL = brick * 100;
+
+                Draw.LineSize = 30;
+                Draw.LineColor = backgroundShine;
+                Draw.Line(new Vector2(0, brickGapL + 20), new Vector2(brickGapL + 20, 5));
+            }
+        }
     }
 
 }
